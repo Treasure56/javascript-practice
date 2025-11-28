21eb45053a25ae9c90825ce8d9dd8a88cc1b5049
treasure56 <simplicitytreasure@gmail.com>
2025-11-28 20:53:32 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const name = "Treasure";
+const num = 20;
+const countries = ["nigeria", "spain", "china"];
+const isLoading = true;
+const data = {
+  name: "mira",
+  age: 16,
+  gender: "female",
+  isAdult: false,
+};
+
+console.log(name);
+console.log(num);
+console.log(countries);
+console.log(isLoading);
+console.log(data);
+
+console.log("----------------------------------------");
+console.log("----------------------------------------");
+
+// using let to create five variables
+
+let age = 16;
+let message = "mira is 16 years old";
+let courses = ["Javascript", "ui/ux", "devops"];
+let isAdult = false;
+let result = {
+  name: "amira",
+  birthday: "12 june 2025",
+  age: 2,
+};
+
+//  reasign the let variables
+
+age = 13;
+message = "Something went wrong";
+courses = ["nigeria", "japan"];
+isAdult = true;
+result = {
+  name: "Tee",
+  birthday: "10 july 2020",
+  age: "5",
+};
+
+console.log(age);
+console.log(message);
+console.log(courses);
+console.log(isAdult);
+console.log(result);
